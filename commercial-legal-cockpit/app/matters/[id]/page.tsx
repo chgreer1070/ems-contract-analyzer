e4fe6656e064510309e32674007d3cacf9aaa893
chgreer1070 <chris.greer@outlook.com>
2026-08-07 18:44:43 -0500

    feat: expose records governance in matter workspace

--- commercial-legal-cockpit/app/matters/[id]/page.tsx
+++ commercial-legal-cockpit/app/matters/[id]/page.tsx
@@ -1,7 +1,8 @@
 import DocumentUploadPanel from "@/components/DocumentUploadPanel";
+import GovernancePanel from "@/components/GovernancePanel";
 import MatterWorkspace from "@/components/MatterWorkspace";
 
 export default async function MatterPage({params}:{params:Promise<{id:string}>}){
   const {id}=await params;
-  return <><DocumentUploadPanel matterId={id}/><MatterWorkspace matterId={id}/></>;
+  return <><DocumentUploadPanel matterId={id}/><GovernancePanel matterId={id}/><MatterWorkspace matterId={id}/></>;
 }
