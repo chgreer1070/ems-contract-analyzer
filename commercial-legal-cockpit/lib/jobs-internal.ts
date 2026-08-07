9e0a34923be2bd40f7ee93a70d6f01238a596529
chgreer1070 <chris.greer@outlook.com>
2026-08-07 18:07:17 -0500

    fix: provide audited worker job exports

---
+++ commercial-legal-cockpit/lib/jobs-internal.ts
@@ -0,0 +1,3 @@
+export { completeJob, continueJob, enqueueJob, failJob, waitExternal } from "@/lib/jobs";
+export type { ProcessingJob } from "@/lib/jobs";
+export { pollAzureOcr } from "@/lib/ocr";
