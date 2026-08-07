c621044f2625801de5b0221683aed1e674df46fd
chgreer1070 <chris.greer@outlook.com>
2026-08-07 18:26:31 -0500

    feat: embed secure source upload in matter workspace

--- commercial-legal-cockpit/app/matters/[id]/page.tsx
+++ commercial-legal-cockpit/app/matters/[id]/page.tsx
@@ -1,6 +1,7 @@
+import DocumentUploadPanel from "@/components/DocumentUploadPanel";
 import MatterWorkspace from "@/components/MatterWorkspace";
 
 export default async function MatterPage({params}:{params:Promise<{id:string}>}){
   const {id}=await params;
-  return <MatterWorkspace matterId={id}/>;
+  return <><DocumentUploadPanel matterId={id}/><MatterWorkspace matterId={id}/></>;
 }
