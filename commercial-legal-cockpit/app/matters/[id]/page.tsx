d169017a05504611718d3d168248573b2c203e3c
chgreer1070 <chris.greer@outlook.com>
2026-08-07 18:55:06 -0500

    feat: expose restricted matter membership controls

--- commercial-legal-cockpit/app/matters/[id]/page.tsx
+++ commercial-legal-cockpit/app/matters/[id]/page.tsx
@@ -1,8 +1,9 @@
 import DocumentUploadPanel from "@/components/DocumentUploadPanel";
 import GovernancePanel from "@/components/GovernancePanel";
+import MatterAccessPanel from "@/components/MatterAccessPanel";
 import MatterWorkspace from "@/components/MatterWorkspace";
 
 export default async function MatterPage({params}:{params:Promise<{id:string}>}){
   const {id}=await params;
-  return <><DocumentUploadPanel matterId={id}/><GovernancePanel matterId={id}/><MatterWorkspace matterId={id}/></>;
+  return <><DocumentUploadPanel matterId={id}/><GovernancePanel matterId={id}/><MatterAccessPanel matterId={id}/><MatterWorkspace matterId={id}/></>;
 }
